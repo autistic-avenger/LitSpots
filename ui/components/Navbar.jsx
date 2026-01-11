6df66e13d0d8528bb26dd535796cca68c93e17e2
Aditya Kumar <aditya4u164@gmail.com>
2026-01-12 03:00:35 +0530

    Navbar update

--- ui/components/Navbar.jsx
+++ ui/components/Navbar.jsx
@@ -13,14 +13,17 @@ const poppins = Poppins({
 function Navbar() {
 
   return (
-    <div className='h-15 w-full bg-[#15191E] flex  justify-center sm:justify-between items-center'>
-        <div className='sm:ml-9 flex justify-centre items-center space-x-1'>
-            <Image alt='MAP' src={"/Map.svg"} width={45} height={45}/>
+    <header className='sticky top-0 z-50 w-full backdrop-blur '>
+
+    <div className='h-15 w-full flex  justify-center sm:justify-between items-center'>
+        <div className='sm:ml-9 flex justify-centre items-center space-x-1 '>
+            <Image alt='MAP' src={"/Map.svg"} width={45} height={45} className='shadow-[0_0_333px_rgba(138,43,226,0.45)]'/>
+
             <span className= "text-[#11F592] font-extrabold text-4xl"> <span className='text-white rota'>L</span>IT SPOTS</span>
         </div>
 
         <div className='mr-9 hidden sm:block'>
-            <button className={"bg-[#11F592] p-5 font-bold text-2xl antialiased text-[#000000] border border-black rounded-full flex justify-center items-center h-8 ${poppins.className} font-bold hover:scale-105 ease-in-out duration-300 space-x-1 cursor-pointer"}
+            <button className={`bg-[#11F592] p-5 font-bold text-2xl antialiased text-[#000000] border border-black rounded-full flex justify-center items-center h-8 ${poppins.className} font-bold hover:scale-105 ease-in-out duration-300 space-x-1 cursor-pointer shadow-[0_0_15px_rgba(0,255,200,0.45)]`}
             >
                 <Image src={"/map.png"} width={24} height={24}/> <span>Open Map</span>
             </button>
@@ -28,6 +31,7 @@ function Navbar() {
 
       
     </div>
+    </header>
   )
 }
 
