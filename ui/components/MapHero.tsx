465e6b72b813cb6145eecd7440511cd0d86dfabb
Aditya Kumar <aditya4u164@gmail.com>
2026-01-12 22:22:02 +0530

    ADD MAP IN HERO

--- ui/components/MapHero.tsx
+++ ui/components/MapHero.tsx
@@ -3,10 +3,11 @@ import Image from 'next/image'
 
 function MapHero() {
   return (
-    <div className='w-full'>
-        <div>
-          <Image alt="MAP" src=""/>
-
+    <div className='w-full flex justify-center mt-19 '>
+        <div className='p-3 rounded-2xl bg-[#05f81949] backdrop-opacity-5'>
+          <div className='overflow-hidden rounded-2xl'>
+            <Image className=' blur-sm overflow-hidden' width={1100} height={800} alt="MAP" src="/MapPlaceholder.png"/>
+            </div>
         </div>
     </div>
   )
