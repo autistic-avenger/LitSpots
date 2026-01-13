0033e4f8ff20ba65e08e0aef60d31c05b452d7f2
Aditya Kumar <aditya4u164@gmail.com>
2026-01-13 18:58:37 +0530

    LOGIN LOGOUT SIGNUP AND VERIFY DONE

---
+++ app/api/users/signup/route.ts
@@ -0,0 +1,45 @@
+import { connectDB } from "@/dbConfig/dbConfig";
+import User from '@/models/userModel.js'
+import {NextResponse,NextRequest } from 'next/server'
+import bcrypt from "bcryptjs";
+import { sendMail } from "@/utils/mailer";
+
+
+connectDB()
+
+export async function POST(req:NextRequest){
+    try {
+        const reqBody = await req.json()
+        const {username,email,password} = reqBody
+        const user = await User.findOne({email})
+        
+        if (user){
+            return NextResponse.json({error:"User Already Exists"},{status:400})
+        }
+        let salt = await bcrypt.genSalt()
+        let encryptedPass = await bcrypt.hash(password,salt)
+
+        const newUser = new User({
+            username,
+            email,
+            password:encryptedPass,
+        })
+        const savedUser = await newUser.save()
+
+        await sendMail({email,emailType:'VERIFY' ,userId:savedUser._id})
+
+        const userObject = savedUser.toObject();
+        delete userObject.password;
+
+        return NextResponse.json({
+            message:"User registered successfully",
+            success:true,
+            userObject
+        })
+        
+        
+    } 
+    catch (error:any) {
+        return NextResponse.json({error:error.message},{status:500})
+    }
+}
---
+++ app/api/users/verifyemail/route.ts
@@ -0,0 +1,28 @@
+import { connectDB } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { NextRequest,NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+import { sendMail } from "@/utils/mailer";
+
+connectDB()
+
+export async function POST(res:NextRequest){
+    try {
+        const reqBody = await res.json()
+        const {token} = reqBody
+
+        const user = await User.findOne({verifyToken:token,verifyTokenExpiry: {$gt:Date.now()}})
+        if (!user){
+            return NextResponse.json({error:"Invalid Token"},{status:400})
+        }
+        user.isVerified = true
+        user.verifyToken = undefined
+        user.verifyTokenExpiry = undefined
+
+        await user.save()
+        return NextResponse.json({message:"Email Verified!"},{status:200})
+    } catch (error:any) {
+        return NextResponse.json({error:error.message},{status:500})
+        
+    }
+}
