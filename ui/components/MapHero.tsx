13b4484dba7333190a0d6e1b63a94502bec5f9f1
Aditya Kumar <aditya4u164@gmail.com>
2026-01-16 03:48:07 +0530

    FIXES

--- ui/components/MapHero.tsx
+++ ui/components/MapHero.tsx
@@ -10,9 +10,9 @@ function MapHero() {
             <Image className=' blur-sm overflow-hidden' width={1100} height={800} alt="MAP" src="/MapPlaceholder.png"/>
             <div className='absolute z-999 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
 
-            <Link href={"/map/live"} className='block text-white font-sans bg-[#11F592] pt-2 pb-2 rounded-2xl  font-bold border-[2.2] border-black text-[1rem] sm:text-3xl pl-4 pr-4 focus:outline-none hover:scale-105 ease-in-out duration-200'>
+            <a href={"/map/live"} className='block text-white font-sans bg-[#11F592] pt-2 pb-2 rounded-2xl  font-bold border-[2.2] border-black text-[1rem] sm:text-3xl pl-4 pr-4 focus:outline-none hover:scale-105 ease-in-out duration-200'>
               OPEN MAP
-            </Link>
+            </a>
             
             </div>
 
