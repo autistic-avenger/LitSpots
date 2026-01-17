79382fcdf11a7628ad7b52d6c4065c9900df2f80
Aditya Kumar <aditya4u164@gmail.com>
2026-01-17 06:42:37 +0530

    Add maps and search funct

--- app/map/live/page.tsx
+++ app/map/live/page.tsx
@@ -1,8 +1,102 @@
-export default function map(){
+"use client"
+import {useState,useRef} from 'react';
+import {Map} from 'react-map-gl/maplibre';
+import 'maplibre-gl/dist/maplibre-gl.css';
+import Image from 'next/image';
+import { MapRef } from 'react-map-gl/maplibre';
+import axios from 'axios';
+import toast from 'react-hot-toast';
 
-    return(
-        <>
-        <h1>HELLO</h1>
-        </>
-    )
+
+interface Cords{
+    lat:string
+    lon:string
+}
+
+export default function MAPS() {
+    
+
+    const [search,setSearch] = useState<string>("")
+    const mapRef = useRef<MapRef | null>(null);
+    
+    async function onSearch(){
+        if(search==""){
+            return;
+        }
+        try{
+            const response = await axios.get(`https://geocode.maps.co/search?q=${search} india&api_key=696ab1f6a9693374084149bug78b173`)
+            toast.loading("Fetching location",{id:"geosearch",position:'bottom-center'})
+            setTimeout(()=>{
+                if (response?.data[0] == undefined){
+                    toast.error("Couldn't find that location",{id:"geosearch",position:'bottom-center'})
+                }else{
+                    toast.success("Location Found",{id:"geosearch",position:'bottom-center'})
+                    const longitude = response?.data[0].lon
+                    const latitude = response?.data[0].lat
+                    mapRef.current?.flyTo({
+                         center: [parseFloat(longitude), parseFloat(latitude)],
+                         zoom: 15,
+                         duration: 4000
+                    });
+
+                }
+            },500)
+            console.log(response?.data[0])
+
+
+            
+
+
+        }catch(err){
+            toast.error("error")
+        }
+    }
+    function handleEnter(e:any){
+        if(e.key=="Enter"&& !e.repeat){
+            onSearch()
+        }
+    }
+    
+  return (
+    <div className='relative w-full h-screen'>
+        <div className='absolute w-full flex justify-center items-center h-15 z-9'>
+
+            <div className='min-w-60 w-140 flex h-13 bg-white  border-3 rounded-4xl m-2 border-[#11F592] z-9 pl-6 overflow-hidden'>
+
+                <input type="text" placeholder='Search' className=' placeholder:text-gray-700 placeholder:mt-1 text-xl h-full w-[80%] outline-none caret-black text-black flex justify-end' 
+
+                onChange={(e)=>{
+                    setSearch(e.target.value)
+                }}
+                value={search}
+                onKeyDownCapture={handleEnter}
+                />
+                <button className=' outline-none w-[20%] h-full flex justify-end items-center'>
+                    {
+                        (search!="")?<Image alt='close' className='mr-2 sm:mr-7' src={'/close.svg'} width={25} onClick={()=>{
+                            setSearch("")
+                        }} height={25}/>:""
+                    }
+                    
+                    <Image alt='search' width={30} height={30} src={'/search.svg'} className='mr-3.5 fill-black'  onClick={onSearch}/>
+                </button>
+
+            </div>
+        </div>
+        <Map
+          ref={mapRef}
+          initialViewState={{
+            longitude: 77.5775,
+            latitude: 12.9629,
+            zoom: 12
+          }}
+          style={{width: '100%', height: '100%'}}
+          mapStyle="https://tiles.openfreemap.org/styles/liberty"
+          cursor='auto'
+          attributionControl= {false}
+
+        >
+        </Map>
+    </div>
+  );
 }
