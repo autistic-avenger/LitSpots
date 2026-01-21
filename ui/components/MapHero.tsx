0a70af9d28beaa4be3af3d2aacc8cbfe5c11ce3d
Aditya Kumar <aditya4u164@gmail.com>
2026-01-21 19:17:55 +0530

    Change Map placeholder and TEST

--- ui/components/MapHero.tsx
+++ ui/components/MapHero.tsx
@@ -7,7 +7,7 @@ function MapHero() {
     <div className='w-full flex justify-center mt-19 '>
         <div className='p-3 rounded-2xl bg-[#05f81949] backdrop-opacity-5'>
           <div className='overflow-hidden rounded-2xl'>
-            <Image className=' blur-sm overflow-hidden' width={1100} height={800} alt="MAP" src="/MapPlaceholder.png"/>
+            <img className=' blur-sm overflow-hidden' width={1100} height={800} alt="MAP" src="/MapPlaceholder.png"/>
             <div className='absolute z-999 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
 
             <a href={"/map/live"} className='block text-white font-sans bg-[#11F592] pt-2 pb-2 rounded-2xl  font-bold border-[2.2] border-black text-[1rem] sm:text-3xl pl-4 pr-4 focus:outline-none hover:scale-105 ease-in-out duration-200'>
